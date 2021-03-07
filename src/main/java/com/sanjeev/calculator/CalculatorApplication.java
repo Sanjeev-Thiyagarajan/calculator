ed894b6f6f78a754c95d85d0f0737be6dc4187bb
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-03-07 01:25:17 -0500

    adfasdf

--- src/main/java/com/sanjeev/calculator/CalculatorApplication.java
+++ src/main/java/com/sanjeev/calculator/CalculatorApplication.java
@@ -2,7 +2,9 @@ package com.sanjeev.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Main Spring Application.
+ */
 @SpringBootApplication
 public class CalculatorApplication {
 
