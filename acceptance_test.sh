f4812ab60c419ba5c0a13200a974977f030443ac
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-03-07 16:03:31 -0500

    asdf

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
